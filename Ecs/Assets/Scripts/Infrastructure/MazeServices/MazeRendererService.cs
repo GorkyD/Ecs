8506388788c6855914ee0c7534ff935b1db586ab
Danila Harkavy <d.garkavyy@mail.ru>
2023-01-04 02:02:25 +0100

    Add parent for walls

--- Ecs/Assets/Scripts/Infrastructure/MazeServices/MazeRendererService.cs
+++ Ecs/Assets/Scripts/Infrastructure/MazeServices/MazeRendererService.cs
@@ -7,31 +7,33 @@ namespace Infrastructure.MazeGenerate
     public class MazeRendererService
     {
         private readonly Transform _wall;
+        private Transform _wallsParent;
         private MazeRendererService()
         {
             AssetProvider assetProvider = new AssetProvider();
             _wall = assetProvider.Load<Transform>(AssetPath.Wall);
+            _wallsParent = Object.FindObjectOfType<WallComponent>().transform;
         }
 
-        public bool Draw(WallState[,] maze, int width, int height)
+        public void Draw(WallState[,] maze, int width, int height)
         {
             for (int i = 0; i < width; i++)
             {
                 for (int j = 0; j < height; j++)
                 {
-                    var cell = maze[i, j];
-                    var position = new Vector3(-width / 2 + i, 0, -height / 2 + j);
+                    WallState cell = maze[i, j];
+                    Vector3 position = new Vector3(-width / 2 + i, 0, -height / 2 + j);
 
                     if (cell.HasFlag(WallState.Up))
                     {
-                        Transform topWall = Object.Instantiate(_wall);
+                        Transform topWall = Object.Instantiate(_wall, _wallsParent);
                         topWall.position = position + new Vector3(0, 0, 0.5f);
                         topWall.localScale = new Vector3(1f, topWall.localScale.y, topWall.localScale.z);
                     }
                     
                     if (cell.HasFlag(WallState.Left))
                     {
-                        Transform leftWall = Object.Instantiate(_wall);
+                        Transform leftWall = Object.Instantiate(_wall, _wallsParent);
                         leftWall.position = position + new Vector3(-0.5f, 0, 0);
                         leftWall.localScale = new Vector3(1f, leftWall.localScale.y, leftWall.localScale.z);
                         leftWall.eulerAngles = new Vector3(0, 90, 0);
@@ -41,26 +43,20 @@ namespace Infrastructure.MazeGenerate
                     {
                         if (cell.HasFlag(WallState.Right))
                         {
-                            Transform rightWall = Object.Instantiate(_wall);
+                            Transform rightWall = Object.Instantiate(_wall, _wallsParent);
                             rightWall.position = position + new Vector3(0.5f, 0, 0);
                             rightWall.localScale = new Vector3(1f, rightWall.localScale.y, rightWall.localScale.z);
                             rightWall.eulerAngles = new Vector3(0, 90, 0);
                         }
                     }
-                    
-                    if (j == 0)
-                    {
-                        if (cell.HasFlag(WallState.Down))
-                        {
-                            Transform downWall = Object.Instantiate(_wall);
-                            downWall.position = position + new Vector3(0, 0, -0.5f);
-                            downWall.localScale = new Vector3(1f, downWall.localScale.y, downWall.localScale.z);
-                        }
-                    }
+
+                    if (j != 0) continue;
+                    if (!cell.HasFlag(WallState.Down)) continue;
+                    Transform downWall = Object.Instantiate(_wall, _wallsParent);
+                    downWall.position = position + new Vector3(0, 0, -0.5f);
+                    downWall.localScale = new Vector3(1f, downWall.localScale.y, downWall.localScale.z);
                 }
             }
-
-            return true;
         }
     }
 }
