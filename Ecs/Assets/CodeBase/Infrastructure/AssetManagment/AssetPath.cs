a157aae951dd9c4babeafbd37b2356539d54f71c
Danila Harkavy <d.garkavyy@mail.ru>
2023-01-07 21:12:01 +0100

    Remove sky

--- Ecs/Assets/CodeBase/Infrastructure/AssetManagment/AssetPath.cs
+++ Ecs/Assets/CodeBase/Infrastructure/AssetManagment/AssetPath.cs
@@ -2,7 +2,8 @@ public static class AssetPath
 {
     public const string JoyStick = "Simple Joystick";
     public const string ChainWeapon = "ChainWeapon";
-    public const string Player = "Player";
     public const string Ground = "Location";
+    public const string Enemy = "EnemyMain";
+    public const string Player = "Player";
     public const string Wall = "Wall";
 }
